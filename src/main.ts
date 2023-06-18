0473bf336bb17ea4ca6c3718fbfa2c90edffb584
Joel Campos <joelcamposoliva9000@gmail.com>
2023-06-18 11:22:03 +0200

    Read config

--- src/main.ts
+++ src/main.ts
@@ -2,19 +2,24 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './api/app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import packageJson from '../package.json';
+import { getConfig } from './utils/config/get-config.util';
 
 async function bootstrap() {
+  const { enableSwagger } = getConfig();
+
   const app = await NestFactory.create(AppModule);
 
-  // Open API
-  const config = new DocumentBuilder()
-    .setTitle(packageJson.name)
-    .setDescription(packageJson.description)
-    .setVersion(packageJson.version)
-    .addBasicAuth()
-    .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('documentation', app, document);
+  if (enableSwagger) {
+    // Open API
+    const config = new DocumentBuilder()
+      .setTitle(packageJson.name)
+      .setDescription(packageJson.description)
+      .setVersion(packageJson.version)
+      .addBasicAuth()
+      .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('documentation', app, document);
+  }
 
   await app.listen(4000);
 }
---
+++ src/utils/config/get-config.util.ts
@@ -0,0 +1,24 @@
+export const getConfig = (): Config => {
+  const { ENABLE_SWAGGER } = process.env as unknown as EnvFile;
+
+  return {
+    enableSwagger: evalEnvBoolean(ENABLE_SWAGGER),
+  };
+};
+
+// Booleans
+
+type EnvBoolean = 'yes' | 'no' | 'true' | 'false';
+const evalEnvBoolean = (envBoolean: EnvBoolean) =>
+  ['yes', 'true'].includes(envBoolean.toLowerCase());
+
+// Models
+
+interface EnvFile {
+  // OPEN API
+  ENABLE_SWAGGER?: EnvBoolean;
+}
+
+interface Config {
+  enableSwagger: boolean;
+}
