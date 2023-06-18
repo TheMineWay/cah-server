e4240bb28c014b1109f0d9944845b4ec73dd1e33
Joel Campos <joelcamposoliva9000@gmail.com>
2023-06-18 11:37:15 +0200

    Rate limit

--- src/api/app.module.ts
+++ src/api/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
-  imports: [],
+  imports: [
+    ThrottlerModule.forRoot({
+      ttl: 60,
+      limit: 1000, // 1000 requests per minute
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+    AppService,
+  ],
 })
 export class AppModule {}
