b6e22256e8d61f73d3f1bdc06f27b74eb55fa241
Joel Campos <joelcamposoliva9000@gmail.com>
2023-06-21 17:56:19 +0200

    Standard exception

--- src/api/auth/auth.service.ts
+++ src/api/auth/auth.service.ts
@@ -1,5 +1,6 @@
-import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { UsersService } from '../../server/users/users.service';
+import { NotFoundException } from '../../exceptions/http/not-found.exception';
 
 @Injectable()
 export class AuthService {
@@ -8,7 +9,7 @@ export class AuthService {
   async login(nick: string, password: string) {
     const user = await this.usersService.findByNick(nick);
 
-    if (!user) throw new HttpException('not found', HttpStatus.NOT_FOUND);
+    if (!user) throw new NotFoundException();
     return null;
   }
 }
