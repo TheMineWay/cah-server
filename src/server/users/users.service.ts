78fb1c08e2ef712d7cfa70858d41194ae4127803
Joel Campos <joelcamposoliva9000@gmail.com>
2023-06-21 17:41:58 +0200

    Basic prisma query

--- src/server/users/users.service.ts
+++ src/server/users/users.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import { uuid } from '../../types/generic/uuid.type';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
 export class UsersService {
+  constructor(private readonly prisma: PrismaService) {}
+
   findById = async (userId: uuid) => {
-    return userId;
+    return await this.prisma.user.findUnique({
+      where: {
+        id: userId,
+      },
+    });
   };
 }
