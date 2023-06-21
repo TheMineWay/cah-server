3a54c6714b229617290b7f0822e12fbd42354768
Joel Campos <joelcamposoliva9000@gmail.com>
2023-06-21 18:35:51 +0200

    Validate

--- src/api/auth/dtos/login.dto.ts
+++ src/api/auth/dtos/login.dto.ts
@@ -1,4 +1,16 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsString, MaxLength, IsNotEmpty } from 'class-validator';
+
 export class LoginDTO {
+  @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(16)
   nick: string;
+
+  @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(128)
   password: string;
 }
