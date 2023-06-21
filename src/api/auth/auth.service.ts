749fd108c495865bdc85547f4e7f95b30c776e3d
Joel Campos <joelcamposoliva9000@gmail.com>
2023-06-21 18:40:20 +0200

    JWT

--- src/api/auth/auth.service.ts
+++ src/api/auth/auth.service.ts
@@ -3,10 +3,14 @@ import { UsersService } from '../../server/users/users.service';
 import { NotFoundException } from '../../exceptions/http/not-found.exception';
 import { compareHashWithSalt } from '../../security/cryptography/cryptography';
 import { WrongPasswordException } from '../../exceptions/auth/wrong-password.exception';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly usersService: UsersService) {}
+  constructor(
+    private readonly usersService: UsersService,
+    private jwtService: JwtService,
+  ) {}
 
   async login(nick: string, password: string) {
     const user = await this.usersService.findByNick(nick);
@@ -16,6 +20,9 @@ export class AuthService {
     if (!compareHashWithSalt(user.password, password))
       throw new WrongPasswordException();
 
-    return null;
+    const payload = { id: user.id, nick: user.nick };
+    return {
+      access_token: await this.jwtService.signAsync(payload),
+    };
   }
 }
